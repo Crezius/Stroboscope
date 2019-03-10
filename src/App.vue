edcdd620a32c2647f3c0bd00b22cb2ab059dfd2f
Crezius <guignard.arnaud@orange.fr>
2019-03-10 15:37:33 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
+
+<style scoped src="@/assets/css/style.css">
+</style>
+
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div :style="{backgroundColor: currentColor}">
+        <p> Mon Stroboscope</p>
+        <button @click="strobos()" style="text-align: center">Start / Stop</button> 
+        <button @click="reset()" style="text-align: center">Reset</button> 
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue';
+    
+
+    export default {
+    
+      name: 'app',
+      components: {
+        HelloWorld,
+      },
+      
+      data(){
+            return{
+            
+                i: 0,
+                currentColor: ``,
+                bgColor: [
+                    `red`,
+                    `yellow`,
+                    `green`,
+                    `blue`,
+                ],
+                timer: null,
+                stop: true,
+                    
+            }
+        },
+        
+        methods:{
+            strobos() {
+                
+                this.start();
+            
+                if(!this.stop){
+                    this.timer = setInterval(this.changeColor, 20);
+                } else {
+                    clearInterval(this.timer);
+                }
+                
+            },
+            
+            start(){
+                this.stop = !this.stop;
+            },
+            
+            changeColor() {
+                this.currentColor = this.bgColor[this.i++%this.bgColor.length];
+            },
+            
+            reset() {
+                this.stop = true;
+                clearInterval(this.timer);
+                this.currentColor = `white`
+            }
+        }
+        
+    };
+
+
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    div {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+
 </style>
